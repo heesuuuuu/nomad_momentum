4aedd8ab2d5ecd5316f2cc388006d026789a6e56
heesuuuuu <heesu.dev@gmail.com>
2025-01-11 16:23:13 +0900

    fix : git folder name change

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("hi");dsd
+const a = 5;
+let b = 2;
+
+let myName = 'nico';
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello! " + myName);
+
+myName = 'nicolas'
+console.log("your name is " + myName);
