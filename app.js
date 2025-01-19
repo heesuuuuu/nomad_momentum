bf1c75f0775a13abc88122a1d0d0b69c4efea22c
heesuuuuu <heesu.dev@gmail.com>
2025-01-19 22:14:49 +0900

    #3.2 : find by id, querySelector

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const a = 5;
-let b = 2;
+// const hellos = document.getElementsByClassName('hello');
+//const title = document.getElementsByTagName('h1');
 
-let myName = 'nico';
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello! " + myName);
-
-myName = 'nicolas'
-console.log("your name is " + myName);
+// querySelector는 데이터 많을 수 있지만 첫번째 값만 가져온다
+//const title = document.querySelector('.hello h1')
+// 값 전체를 가져오고 싶다면 selectorAll을 사용한다
+const title = document.querySelectorAll('.hello h1')
+console.log(title)
