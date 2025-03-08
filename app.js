6cd01465445e09446aa6f90bc816e889129d8f0b
heesuuuuu <heesu.dev@gmail.com>
2025-03-08 14:47:59 +0900

    #4.3 : Link를 클릭했을 때 발생하는 이벤트에 대한 설명

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a")
+
 function onLoginSubmit(event){
     event.preventDefault();
     console.log(loginInput.value);
 }
 
-loginForm.addEventListener("submit",onLoginSubmit); // 주의 : onLoginSubmit() 이렇게 쓰면 안됨 
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event);
+    alert("clicked!");
+}
 
+loginForm.addEventListener("submit",onLoginSubmit); // 주의 : onLoginSubmit() 이렇게 쓰면 안됨 
+link.addEventListener("click",handleLinkClick);
