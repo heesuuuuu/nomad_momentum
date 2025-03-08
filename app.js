92f5fab8dfb167fd570fd9ce687a8e52c1473fe6
heesuuuuu <heesu.dev@gmail.com>
2025-03-08 15:27:02 +0900

    #4.4 : hidden 클래스와 삭제와 추가를 통한 결과물 호출

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a")
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    // greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`; //위와 같은 의미 
+    greeting.classList.remove(HIDDEN_CLASSNAME); //정보를 담은거라서 대문자로 관습처럼 사용
 }
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.dir(event);
-    alert("clicked!");
-}
 
 loginForm.addEventListener("submit",onLoginSubmit); // 주의 : onLoginSubmit() 이렇게 쓰면 안됨 
-link.addEventListener("click",handleLinkClick);
+ 
